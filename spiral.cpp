a27c252dc36b53afc0eb954d570e9ba66a1cca3c
fahadkhan02222 <mohdfahad0306@gmail.com>
2026-03-17 12:02:27 +0530

    Create spiral.cpp

---
+++ spiral.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int r,c;
+    cin>>r>>c;
+    vector<vector<int>> a(r, vector<int>(c));
+
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            cin>>a[i][j];
+
+    int top=0,bottom=r-1,left=0,right=c-1;
+
+    while(top<=bottom && left<=right){
+        for(int i=left;i<=right;i++) cout<<a[top][i]<<" ";
+        top++;
+
+        for(int i=top;i<=bottom;i++) cout<<a[i][right]<<" ";
+        right--;
+
+        if(top<=bottom){
+            for(int i=right;i>=left;i--) cout<<a[bottom][i]<<" ";
+            bottom--;
+        }
+
+        if(left<=right){
+            for(int i=bottom;i>=top;i--) cout<<a[i][left]<<" ";
+            left++;
+        }
+    }
+}
