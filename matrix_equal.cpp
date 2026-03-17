d945a88ef64e3c7637a084ca6e8eee1f9417d19a
fahadkhan02222 <mohdfahad0306@gmail.com>
2026-03-17 12:05:04 +0530

    Create matrix_equal.cpp

---
+++ matrix_equal.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<vector<int>> A={{1,1},{2,2}};
+    vector<vector<int>> B={{1,1},{2,2}};
+
+    if(A==B) cout<<"Identical";
+    else cout<<"Not Identical";
+}
