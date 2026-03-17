657f596a23a19681bb59129f3c8100b9fe70adef
fahadkhan02222 <mohdfahad0306@gmail.com>
2026-03-17 12:12:01 +0530

    Create merge.cpp

---
+++ merge.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> a={1,2,3,0,0,0};
+    vector<int> b={2,5,6};
+    int m=3,n=3;
+
+    int i=m-1,j=n-1,k=m+n-1;
+
+    while(i>=0 && j>=0){
+        if(a[i]>b[j]) a[k--]=a[i--];
+        else a[k--]=b[j--];
+    }
+
+    while(j>=0) a[k--]=b[j--];
+
+    for(int x:a) cout<<x<<" ";
+}
